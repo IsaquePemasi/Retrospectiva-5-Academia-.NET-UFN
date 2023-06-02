3d193d8fcbfac58299eb7806bdfaba9cccb46a5b
IsaquePemasi <ADSisaque@gmail.com>
2023-06-02 20:32:34 -0300

    add

---
+++ a0602/XamarinFormsM1/XamarinFormsM1/CalculadoraSimples.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsM1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculadoraSimples : ContentPage
+    {
+        public CalculadoraSimples()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            double a = 0, b= 0 , t = 0;
+            a = double.Parse(etNumero1.Text);
+            b = double.Parse(etNumero2.Text);
+            if (bt.Text == "+")
+            {
+                t = a + b;
+            }
+            if (bt.Text == "-")
+            {
+                t = a - b;
+            }
+            if (bt.Text == "*")
+            {
+                t = a * b;
+            }
+            if (bt.Text == "/")
+            {
+                t = a / b;
+            }
+            lbResposta.Text = t.ToString();
+        }
+    }
+}
---
+++ a0602/XamarinFormsM1/XamarinFormsM1/ParOuImpar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsM1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParOuImpar : ContentPage
+    {
+        public ParOuImpar()
+        {
+            InitializeComponent();
+            btExecutar.Clicked += btExecutar_Cliked;
+        }
+
+        private void btExecutar_Cliked(object sender, EventArgs e)
+        {
+            int n = int.Parse(etNumero.Text);
+            string res = $"O número {n} é ";
+            if (n%2==0)
+            {
+                res += "Par";  
+            }
+            else
+            {
+                res += "Impar";
+            }
+            lbResposta.Text = res;
+        }
+    }
+}
