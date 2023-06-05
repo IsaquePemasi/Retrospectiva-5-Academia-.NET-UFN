3937b1d34e6e7940b90a76a22ba9569f90137167
IsaquePemasi <ADSisaque@gmail.com>
2023-06-05 17:56:08 -0300

    add

--- a0605/WebApplication1/Controllers/OiMundiController.cs
+++ a0605/WebApplication1/Controllers/OiMundiController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace WebApplication1.Controllers
 {
     public class OiMundiController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        public string Index()
         {
-            return View();
+            //return View();
+            return "Apresenta esta mesnagem" ;
+        }
+
+        public string Welcome(string nome, int vezes =1)
+        {
+            return HtmlEncoder.Default.Encode($"Oi {nome}, vezes: {vezes}");
+        }
+
+        public string Oi()
+        {
+            return "Oi";
         }
     }
 }
