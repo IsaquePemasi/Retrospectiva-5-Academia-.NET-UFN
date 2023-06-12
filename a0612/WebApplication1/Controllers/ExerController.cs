3b4629925384a1318b7c326cdb5e932448b070fd
IsaquePemasi <ADSisaque@gmail.com>
2023-06-12 17:27:32 -0300

    add

---
+++ a0612/WebApplication1/Controllers/ExerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ExerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ a0612/WebApplication1/Controllers/PessoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public async Task <IActionResult> getAllAsync(
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
